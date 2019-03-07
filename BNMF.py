079125a97fdc620011fdbc8bf279008a957711d0
chrisquince <c.quince@warwick.ac.uk>
2019-03-07 17:42:13 +0000

    Update

--- BNMF.py
+++ BNMF.py
@@ -2,6 +2,9 @@ import sys, os
 import argparse
 import numpy as np
 
+from BNMTF_ARD.code.models.bnmf_vb import bnmf_vb
+from BNMTF_ARD.code.cross_validation.mask import compute_folds_attempts
+
 
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -35,7 +38,40 @@ def main(argv):
     for unitig in unitigs:
         X[i,:] = np.asarray(covMap[unitig])
         i += 1
-        
+    
+    no_folds = 10
+    values_K = [1,2,3,4,6,8,10,15,20,30,40]
+
+    output_folder = "./BNMTF_ARD/"
+    output_file = output_folder+'nmf_vb_ard.txt'
+    
+    metrics = ['MSE', 'R^2', 'Rp']
+
+    ''' Model settings. '''
+    iterations = 200
+
+    init_UV = 'random'
+    ARD = True
+
+    lambdaU, lambdaV = 0.1, 0.1
+    alphatau, betatau = 1., 1.
+    alpha0, beta0 = 1., 1.
+    hyperparams = { 'alphatau':alphatau, 'betatau':betatau, 'alpha0':alpha0, 'beta0':beta0, 'lambdaU':lambdaU, 'lambdaV':lambdaV }
+
+
+    ''' Load in data. '''
+    R = X
+    M = np.ones(X.shape)
+    I, J = M.shape
+
+
+    ''' Generate matrices M - one list of M's for each value of K. '''
+    M_attempts = 1000
+    all_Ms_training_and_test = [
+        compute_folds_attempts(I=I,J=J,no_folds=no_folds,attempts=M_attempts,M=M)
+        for K in values_K
+    ]
+    
     print "Dummy"
     
 if __name__ == "__main__":
