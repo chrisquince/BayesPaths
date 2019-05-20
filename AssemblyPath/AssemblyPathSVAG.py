8491f1d7f744dafaf6ef368b98d142636ca2a382
chrisquince <c.quince@warwick.ac.uk>
2019-05-20 19:34:52 +0100

    Update

--- AssemblyPath/AssemblyPathSVAG.py
+++ AssemblyPath/AssemblyPathSVAG.py
@@ -1013,7 +1013,8 @@ class AssemblyPathSVA():
             #update phi marginals
             if removeRedundant:
                 if iter > 50 and iter % 10 == 0:
-                    self.removeRedundant(self.minIntensity, 10,relax_path,uncertainFactor)
+                    if self.G > 1:
+                        self.removeRedundant(self.minIntensity, 10,relax_path,uncertainFactor)
             
             for g in range(self.G):
                 
@@ -1939,15 +1940,15 @@ class AssemblyPathSVA():
         if np.min(sumIntensity[0:self.G]) < minIntensity:
             removed[np.argmin(sumIntensity[0:self.G])] = True
  
-        #if np.sum(removed == True) == 0:
-        for g in range(self.G):
+        if np.sum(removed == True) == 0:
+            for g in range(self.G):
         
-            if removed[g] == False:    
+                if removed[g] == False:    
 
-                for h in range(g+1,self.G):
-                    if dist[g,h] == 0 and removed[h] == False:
-                        removed[h] = True
-                        break    
+                    for h in range(g+1,self.G):
+                        if dist[g,h] == 0 and removed[h] == False:
+                            removed[h] = True
+                            break    
        
         retained = np.logical_not(removed)
         if self.NOISE:
