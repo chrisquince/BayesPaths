e4f2c65fa23d99dcc18a1b5c3b67eefabd2922b7
chrisquince <c.quince@warwick.ac.uk>
2019-07-21 13:53:50 +0100

    Updaye

--- AssemblyPath/AssemblyPathSVAS.py
+++ AssemblyPath/AssemblyPathSVAS.py
@@ -103,6 +103,63 @@ def findSuperBubble(directedGraph, source, descendents, parents):
                 
     return (visited, sink)
 
+"""
+This module implements the Lowess function for nonparametric regression.
+Functions:
+lowess Fit a smooth nonparametric regression curve to a scatterplot.
+For more information, see
+William S. Cleveland: "Robust locally weighted regression and smoothing
+scatterplots", Journal of the American Statistical Association, December 1979,
+volume 74, number 368, pp. 829-836.
+William S. Cleveland and Susan J. Devlin: "Locally weighted regression: An
+approach to regression analysis by local fitting", Journal of the American
+Statistical Association, September 1988, volume 83, number 403, pp. 596-610.
+"""
+
+# Authors: Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
+#
+# License: BSD (3-clause)
+
+from math import ceil
+import numpy as np
+from scipy import linalg
+
+
+def lowess(x, y, f=2. / 3., iter=3):
+    """lowess(x, y, f=2./3., iter=3) -> yest
+    Lowess smoother: Robust locally weighted regression.
+    The lowess function fits a nonparametric regression curve to a scatterplot.
+    The arrays x and y contain an equal number of elements; each pair
+    (x[i], y[i]) defines a data point in the scatterplot. The function returns
+    the estimated (smooth) values of y.
+    The smoothing span is given by f. A larger value for f will result in a
+    smoother curve. The number of robustifying iterations is given by iter. The
+    function will run faster with a smaller number of iterations.
+    """
+    n = len(x)
+    r = int(ceil(f * n))
+    h = [np.sort(np.abs(x - x[i]))[r] for i in range(n)]
+    w = np.clip(np.abs((x[:, None] - x[None, :]) / h), 0.0, 1.0)
+    w = (1 - w ** 3) ** 3
+    yest = np.zeros(n)
+    delta = np.ones(n)
+    for iteration in range(iter):
+        for i in range(n):
+            weights = delta * w[:, i]
+            b = np.array([np.sum(weights * y), np.sum(weights * y * x)])
+            A = np.array([[np.sum(weights), np.sum(weights * x)],
+                          [np.sum(weights * x), np.sum(weights * x * x)]])
+            beta = linalg.solve(A, b)
+            yest[i] = beta[0] + beta[1] * x[i]
+
+        residuals = y - yest
+        s = np.median(np.abs(residuals))
+        delta = np.clip(residuals / (6.0 * s), -1, 1)
+        delta = (1 - delta ** 2) ** 2
+
+    return yest
+
+
 
 class AssemblyPathSVA():
     """ Class for structured variational approximation on Assembly Graph"""    
@@ -111,7 +168,7 @@ class AssemblyPathSVA():
     minLogQGamma = 1.0e-100
         
     def __init__(self, prng, assemblyGraphs, source_maps, sink_maps, G = 2, maxFlux=2, 
-                readLength = 100, epsilon = 1.0e5, epsilonNoise = 1.0e-3, alpha=1.,beta=1.,alpha0=1.0e-9,beta0=1.0e-9,
+                readLength = 100, epsilon = 1.0e5, epsilonNoise = 1.0e-3, alpha=0.1,beta=0.1,alpha0=1.0e-9,beta0=1.0e-9,
                 no_folds = 10, ARD = False, BIAS = True, NOISE = True, muTheta0 = 1.0, tauTheta0 = 100.0,
                 minIntensity = None, fgExePath="./runfg_source/", tauType = 'None', nTauCats = 1, tauThresh = 0.1, bReassign = False,
                 working_dir="/tmp", minSumCov = None, fracCov = None, noiseFrac = 0.03):
@@ -1084,11 +1141,13 @@ class AssemblyPathSVA():
 
         square_diff_matrix = self.exp_square_diff_matrix()  
 
-        logExpTau = (self.alpha + 0.5)/(self.beta + square_diff_matrix)
+        logExpTau = np.log((self.alpha + 0.5)/(self.beta + 0.5*square_diff_matrix))
         
-        lowess_sm = sm.nonparametric.lowess
+        logExpTau1D = np.ravel(logExpTau)
         
-        yest_sm = lowess_sm(np.ravel(logExpTau),np.ravel(self.logX),frac=1./3.,it=3, return_sorted = False)
+        logX1D = np.ravel(self.logX)
+            
+        yest_sm = lowess(logX1D,ogExpTau1D, f=0.25, iter=3)
         
         self.expLogTau = np.reshape(yest_sm ,(self.V,self.S))
         
