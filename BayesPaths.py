5a76308a62a375a1b1723017b08e68b99a6869f9
chrisquince <c.quince@warwick.ac.uk>
2019-11-05 16:25:46 +0000

    Update

--- BayesPaths.py
+++ BayesPaths.py
@@ -462,7 +462,7 @@ def main(argv):
             for g in range(1,Gopt + 1):
                 mean_elbo = np.mean(elbos[g])        
                 mean_err = np.mean(errs[g])
-                means_errs[g - 1] = mean_err
+                mean_errs[g - 1] = mean_err
                 mean_errP = np.mean(errsP[g])   
                 mean_div = np.mean(divs[g]) 
                 mean_divF = np.mean(divFs[g])     
@@ -472,23 +472,24 @@ def main(argv):
                 print(str(g) +"," + str(mean_elbo) +"," + str(mean_err) + "," + str(mean_errP) + "," + str(mean_div) + "," + str(mean_divF) + "," + str(median_ll) + "," + str(median_h))
     
         #Rerun with optimal g
-        
-        print("Using " + str(minG) + " strains")
-        
-        minG = np.argmin(mean_errs) + 1
-        
-        assGraph = AssemblyPathSVA(prng, assemblyGraphsSelect, source_maps_select, sink_maps_select, G = minG, readLength=args.readLength,
-                                        ARD=True,BIAS=args.bias, fgExePath=args.executable_path, bLoess = args.loess, bGam = args.usegam, bLogTau = args.bLogTau, bFixedTau = args.bFixedTau, 
-                                        fracCov = args.frac_cov, noiseFrac = args.noise_frac)
     
-        assGraph.update(args.iters, True,logFile=args.outFileStub + "_log5.txt",drop_strain=None,relax_path=False,bMulti=True)
+        minG = np.argmin(mean_errs) + 1
+       
+        print("Using " + str(minG) + " strains")
+ 
+        assGraph = AssemblyPathSVA(prng, assemblyGraphsSelect, source_maps_select, sink_maps_select, G = minG, readLength=args.readLength,
+                                        ARD=False,BIAS=args.bias, fgExePath=args.executable_path, bLoess = args.loess, bGam = args.usegam, bLogTau = args.bLogTau, bFixedTau = args.bFixedTau, 
+                                        fracCov = args.frac_cov, noiseFrac = args.noise_frac)
+   
+        assGraph.initNMF()
 
-        assGraph.update(args.iters, True,logFile=args.outFileStub + "_log5.txt",drop_strain=None,relax_path=args.relax_path)
-
-        assGraph.update(args.iters, True,logFile=args.outFileStub + "_log6.txt",drop_strain=None,relax_path=False,uncertainFactor=args.uncertain_factor)
+        assGraph.update(args.iters, True,logFile=args.outFileStub + "_log6.txt",drop_strain=None,relax_path=False,bMulti=True)
 
         assGraph.update(args.iters, True,logFile=args.outFileStub + "_log6.txt",drop_strain=None,relax_path=args.relax_path)
-    
+
+        assGraph.update(args.iters, True,logFile=args.outFileStub + "_log7.txt",drop_strain=None,relax_path=False,uncertainFactor=args.uncertain_factor)
+        assGraph.update(args.iters, True,logFile=args.outFileStub + "_log7.txt",drop_strain=None,relax_path=args.relax_path)
+  
     
     assGraph.writeOutput(args.outFileStub + "_Q", False, selectedSamples)
     
