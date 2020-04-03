df9661d2cc947bae2baacda0021d923398361ac2
chrisquince <cquince@warwick.ac.uk>
2020-04-03 18:46:43 +0100

    Update

---
+++ GraphExample.py
@@ -0,0 +1,123 @@
+from itertools import compress
+import argparse
+import sys
+import numpy as np
+import os
+import subprocess
+import re 
+
+from subprocess import PIPE
+from Bio import SeqIO
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
+from collections import defaultdict
+
+from Utils.UnitigGraph import UnitigGraph
+from Utils.UtilsFunctions import convertNodeToName
+from Utils.UtilsFunctions import expNormLogProb
+from Utils.UtilsFunctions import expLogProb
+from kmedoids.kmedoids import kMedoids
+from operator import itemgetter
+
+import uuid
+import networkx as nx
+    
+def readCogStopsDead(cog_graph,kmer_length,cov_file):
+
+    deadEndFile = cog_graph[:-3] + "deadends"
+        
+    stopFile = cog_graph[:-3] + "stops"
+
+    try:
+        unitigGraph = UnitigGraph.loadGraphFromGfaFile(cog_graph,int(kmer_length), cov_file, tsvFile=True, bRemoveSelfLinks = True)
+    except IOError:
+        print('Trouble using file {}'.format(cog_graph))
+        sys.exit()
+    
+        
+    deadEnds = []
+
+
+    try:
+        with open(deadEndFile) as f:
+            for line in f:
+                line.strip()
+                deadEnds.append(line)
+    except IOError:
+        print('Trouble using file {}'.format(deadEndFile))
+        sys.exit()
+        
+    stops = []
+        
+    try:
+        with open(stopFile) as f:
+            for line in f:
+                line = line.strip()
+                toks = line.split("\t")
+                dirn = True
+                if toks[1] == '-':
+                    dirn = False
+                stops.append((toks[0],dirn))
+    except IOError:
+        print('Trouble using file {}'.format(stopFile))
+        sys.exit()
+        
+    return (unitigGraph, stops, deadEnds )
+ 
+ 
+#def removeDegenerate(haplotypes,paths):
+
+
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("cog_graph", help="gfa file")
+    
+    parser.add_argument("kmer_length", help="kmer length assumed overlap")
+    
+    parser.add_argument("cov_file", help="tsv file")
+    
+    args = parser.parse_args()
+
+    import ipdb; ipdb.set_trace()    
+
+    (unitigGraph, stops, deadEnds ) = readCogStopsDead(args.cog_graph,args.kmer_length,args.cov_file)
+    
+    
+    (source_list, sink_list) = unitigGraph.selectSourceSinksStops(stops, deadEnds, 3000)
+
+    source_names = [convertNodeToName(source) for source in source_list] 
+
+    sink_names = [convertNodeToName(sink) for sink in sink_list]
+    
+    unitigGraph.setDirectedBiGraphSource(source_names,sink_names)
+    
+    nx.write_adjlist(unitigGraph.directedUnitigBiGraphS,"diGraphS.adjlist")
+    
+    
+    adjLengths = {}
+    
+    covMapAdj = {}
+    
+    readLength = 150.
+    
+    kFactor = readLength/(readLength - unitigGraph.overlapLength + 1.)
+    
+    for unitig in unitigGraph.unitigs:
+ 
+        adjLengths[unitig] =  unitigGraph.lengths[unitig] - 2.0*unitigGraph.overlapLength + readLength
+                    
+        covMapAdj[unitig] = unitigGraph.covMap[unitig] * float(adjLengths[unitig])*(kFactor/readLength)
+    
+    with open('coverage.csv','w') as f:            
+        for unitig in unitigGraph.unitigs:
+            readSum = np.sum(covMapAdj[unitig])
+        
+            covSum = np.sum(unitigGraph.covMap[unitig])*kFactor
+            
+            f.write(unitig + ',' + str(covSum) + ',' + str(readSum) + '\n') 
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
