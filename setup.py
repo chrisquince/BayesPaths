92f14e62c3d799bdc4fffa73af644fb062293fd8
chrisquince <cquince@warwick.ac.uk>
2020-06-11 18:26:05 +0100

    Update

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="BayesPaths",
+    version="0.1",
+    packages=["BayesPaths"],
+    scripts=["bin/bayespaths"],
+    install_requires=["numpy>=1.15.4","scipy>=1.0.0","pandas>=0.24.2","networkx>=2.4","sklearn","pygam>=0.8.0","gfapy"],
+)
