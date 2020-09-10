5fcfce918aabac2f1cff4ffa8f1875f97066cfb2
chrisquince <cquince@warwick.ac.uk>
2020-09-10 17:46:03 +0100

    Update

--- Utils/PropBin.py
+++ Utils/PropBin.py
@@ -3,6 +3,7 @@ import sys
 from BayesPaths.UnitigGraph import UnitigGraph
 from BayesPaths.UtilsFunctions import convertNodeToName
 from collections import defaultdict
+from collections import Counter
 import networkx as nx
 
 
@@ -26,9 +27,10 @@ def main(argv):
     import ipdb; ipdb.set_trace()
     
     with open(args.list_mags,'r') as cog_file:
-        mags = {line.rstrip() for line in cog_file}
+        mags = {int(line.rstrip()) for line in cog_file}
 
 
+    color_map = {}
     unitig_ass = defaultdict(lambda: -1)
     with open(args.unitig_ass,'r') as cog_file:
         for line in cog_file:
@@ -37,21 +39,57 @@ def main(argv):
             toks = line.split(',')
             
             unitig_ass[toks[0]] = int(toks[2])
-
+            if int(toks[2]) not in color_map: 
+                color_map[int(toks[2])] = toks[1] 
     unitigGraph = UnitigGraph.loadGraphFromGfaFile(args.gfa_file,int(args.kmer_length), args.cov_file)
 
-    for unitig in ['2297102']:
+    unitigNew = {}
+    magAss = defaultdict(set)
+    for unitig in unitigGraph.unitigs:
     
         if unitig_ass[unitig] not in mags:
-            print('Debug')
+            
 
             fNode = unitig + '+'
-            for succ in nx.bfs_tree(unitigGraph.directedUnitigBiGraph, fNode, 10):
-            
-                ass_bin = unitig_ass[succ[:-1]]
+            binCounts = Counter()
+            total = 0.
+            for succ in nx.bfs_successors(unitigGraph.directedUnitigBiGraph, fNode, 20):
+                
+                sUnitig = succ[0][:-1]
+                ass_bin = unitig_ass[sUnitig]
+                
+                if ass_bin in mags:
+                    total += unitigGraph.lengths[sUnitig]
+                    binCounts[ass_bin] += unitigGraph.lengths[sUnitig]
 
-    
+            rNode = unitig + '-'
+            for succ in nx.bfs_successors(unitigGraph.directedUnitigBiGraph, rNode, 20):
+                sUnitig = succ[0][:-1]
+                ass_bin = unitig_ass[sUnitig]
 
+                if ass_bin in mags:
+                    total += unitigGraph.lengths[sUnitig]
+                    binCounts[ass_bin] += unitigGraph.lengths[sUnitig]
+
+
+            if total > 0.:
+                new_ass = binCounts.most_common()[0][0]
+        
+                new_color = color_map[new_ass]
+                unitigNew[unitig] = new_ass
+                magAss[new_ass].add(unitig)
+                #print(unitig + "," + new_color + "," + str(new_ass))    
+        else:
+            mag = unitig_ass[unitig]
+
+            new_color = color_map[mag]
+            unitigNew[unitig] = mag
+            magAss[mag].add(unitig)
+            #print(unitig + "," + new_color + "," + str(mag))
     
+    graph14 = unitigGraph.createUndirectedGraphSubset(list(magAss[14]))
+    graph14.writeToGFA('graph14.gfa')
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
